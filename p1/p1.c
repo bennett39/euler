5eda8adb30e5729f63f557f1f5ab711b7c85ad55
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-06 12:47:48 -0500

    Implement function structure

--- p1/p1.c
+++ p1/p1.c
@@ -1,5 +1,8 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include "../minunit.h"
+
+int sum_of_multiples(int floor, int remainder);
 
 int main(int argc, char *argv[]) {
     /* argv[1] exists */
@@ -18,8 +21,14 @@ int main(int argc, char *argv[]) {
     
     int floor = x / 15;
     int remainder = x % 15;
+    int total = sum_of_multiples(floor, remainder);
 
-    printf("%d\nFloor: %d\nRemainder: %d\n", x, floor, remainder);
+    printf("NUMBER: %d\nFloor: %d\nRemainder: %d\nTotal: %d\n", \
+            x, floor, remainder, total);
     
+    return 0;
+}
+
+int sum_of_multiples(int floor, int remainder) {
     return 0;
 }
