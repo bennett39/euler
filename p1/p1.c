7a6167d8686c5b1a8dd866f70e8204ff98b0a905
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-06 13:10:06 -0500

    Implement tests in p1

--- p1/p1.c
+++ p1/p1.c
@@ -2,33 +2,46 @@
 # include <stdlib.h>
 # include "../minunit.h"
 
-int sum_of_multiples(int floor, int remainder);
+/* Program functions */
+int sum_of_multiples(int x);
+
+/* Testing functions */
+static char * test_2();
+static char * all_tests();
+
+/* Test counter. Incremented by minunit.h */
+int tests_run = 0;
 
 int main(int argc, char *argv[]) {
-    /* argv[1] exists */
-    if (argc != 2) {
-        fprintf(stderr, "No or too many command line arguments\n");
-        return 1;
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
     }
-    
-    /* atoi will return 0 for any non-integer */
-    int x = atoi(argv[1]);
-    
-    if (x <= 0) {
-        fprintf(stderr, "Enter a non-negative integer\n");
-        return 1;
+    else {
+        printf("ALL TESTS PASSED\n");
     }
-    
+    printf("Tests run %d\n", tests_run);
+
+    return result != 0;
+}
+
+int sum_of_multiples(int x) {
     int floor = x / 15;
     int remainder = x % 15;
-    int total = sum_of_multiples(floor, remainder);
+    int total = 0;
 
-    printf("NUMBER: %d\nFloor: %d\nRemainder: %d\nTotal: %d\n", \
+    printf("NUMBER: %d\tFloor: %d\tRemainder: %d\tTotal: %d\n", \
             x, floor, remainder, total);
-    
+
+    return total;
+}
+
+static char * test_2() {
+    mu_assert("error 2 !>> 0", sum_of_multiples(2) == 0);
     return 0;
 }
 
-int sum_of_multiples(int floor, int remainder) {
+static char * all_tests() {
+    mu_run_test(test_2);
     return 0;
 }
