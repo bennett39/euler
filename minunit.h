1705a19e9c358b8a4ab7326c0221181909405b7c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-06 12:31:27 -0500

    Rename unit testing framework

--- minunit.h
+++ minunit.h
@@ -1,3 +1,8 @@
+/* 
+ * MinUnit test framework for C. 
+ * Docs: http://www.jera.com/techinfo/jtns/jtn002.html
+*/
+
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; \
                                      if (message) return message; } while (0)
