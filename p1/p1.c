d6b59bb97cdb5f065ba2cc000bc7f6f4b3d9345a
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-06 17:18:46 -0500

    Implement straightforward for loop solution

--- p1/p1.c
+++ p1/p1.c
@@ -3,7 +3,7 @@
 # include "../minunit.h"
 
 /* Program functions */
-int sum_of_multiples(int total, int x);
+int sum_of_multiples(int x);
 
 /* Testing functions */
 static char * all_tests();
@@ -31,71 +31,62 @@ int main() {
      * all_tests() below, until a test fails and all_tests quits. 
     */
     printf("Tests run %d\n", tests_run);
-    
-    int user_entry, error_check;
-    printf("Enter a number: ");
-    error_check = scanf("%d", &user_entry);
-
-    if (error_check == EOF || error_check == 0) {
-        fprintf(stderr, "Invalid entry. Quitting...\n");
-    }
-    else {
-        printf("You entered %d\n", user_entry);
-        printf("The sum of 3/5 factors is %d\n", sum_of_multiples(0, user_entry));
-    }
 
     return result != 0;
 }
 
-int sum_of_multiples(int total, int x) {
-    x -= 1;
-    if (x < 3) {
-        printf("%d\n", total);
-        return total;
+int sum_of_multiples(int x) {
+    /* Since we're only going to 1000, the total likely won't exceed
+     * 1,000,000, so we're good to use an int. */
+    int total = 0;
+    for (int i = 0; i < x; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            total += i;
+        }
     }
-    else if (x % 3 == 0 || x % 5 == 0) {
-        return sum_of_multiples(total + x, x);
-    }
-    else {
-        return sum_of_multiples(total, x);
-    }
+    return total;
 }
 
 
 /* TESTS */
 
 static char * test_2() {
-    mu_assert("error 2 !>> 0", sum_of_multiples(0, 2) == 0);
+    mu_assert("error 2 !>> 0", sum_of_multiples(2) == 0);
     return 0;
 }
 
 static char * test_10() {
-    mu_assert("error 10 !>> 23", sum_of_multiples(0, 10) == 23);
+    mu_assert("error 10 !>> 23", sum_of_multiples(10) == 23);
     return 0;
 }
 
 static char * test_16() {
-    mu_assert("error 16 !>> 60", sum_of_multiples(0, 16) == 60);
+    mu_assert("error 16 !>> 60", sum_of_multiples(16) == 60);
     return 0;
 }
 
 static char * test_19() {
-    mu_assert("error 19 !>> 78", sum_of_multiples(0, 19) == 78);
+    mu_assert("error 19 !>> 78", sum_of_multiples(19) == 78);
     return 0;
 }
 
 static char * test_31 () {
-    mu_assert("error 31 !>> 225", sum_of_multiples(0, 31) == 225);
+    mu_assert("error 31 !>> 225", sum_of_multiples(31) == 225);
     return 0;
 }
 
 static char * test_33() {
-    mu_assert("error 33 !>> 225", sum_of_multiples(0, 33) == 225);
+    mu_assert("error 33 !>> 225", sum_of_multiples(33) == 225);
     return 0;
 }
 
 static char * test_48() {
-    mu_assert("error 48 !>> 543", sum_of_multiples(0, 47) == 495);
+    mu_assert("error 48 !>> 543", sum_of_multiples(47) == 495);
+    return 0;
+}
+
+static char * test_1000() {
+    mu_assert("error 1000 !>> 233,168", sum_of_multiples(1000) == 233168);
     return 0;
 }
 
@@ -107,6 +98,7 @@ static char * all_tests() {
     mu_run_test(test_31);
     mu_run_test(test_33);
     mu_run_test(test_48);
+    mu_run_test(test_1000);
 
     return 0;
 }
