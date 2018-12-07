6c3bf4543a487cd1ca1f85481254654172ca777f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-07 10:31:55 -0500

    Implement basic framework and unit testing

---
+++ p2/p2.c
@@ -0,0 +1,107 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "../minunit.h"
+
+/* Program functions */
+
+/* Testing functions */
+static char * all_tests();
+
+/* Test counter. Incremented by minunit.h */
+int tests_run = 0;
+
+int main() {
+    /* 
+     * The reults of all_tests() from mintest.h will == 0 when all 
+     * tests pass.
+     * Otherwise, all_tests() will return a string describing the 
+     * failure. 
+    */
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    
+    /* 
+     * mintest.h counts the number of tests it ran, in order listed in
+     * all_tests() below, until a test fails and all_tests quits. 
+    */
+    printf("Tests run %d\n", tests_run);
+
+    return result != 0;
+}
+
+int sum_even_fibs(long long total, int last, int current, int upper_bound) {
+    /* Since this will eventually add terms up to 4 million, a
+     * conservative upper bound is 4 million * 4 million (aka 4
+     * trillion). That's too big for an int. While we won't likely hit
+     * the limit, I'll use a long long to store the sum */
+
+    /* Recursive Fibonacci solution */
+    if (total >= upper_bound) {
+        printf("%lli", total);
+        return total;
+    }
+    
+    int new = last + current;
+    if (new % 2 == 0) {
+        total += new;
+    }
+    last = current;
+    current = new;
+    return sum_even_fibs(total, last, current, upper_bound);
+}
+
+
+/* TESTS */
+
+static char * test_10() {
+    mu_assert("error 10 !>> 10", sum_even_fibs(0, 1, 1, 10) == 10);
+    return 0;
+}
+
+static char * test_50() {
+    mu_assert("error 50 !>> 44", sum_even_fibs(0, 1, 1, 50) == 44);
+    return 0;
+}
+/*
+static char * test_16() {
+    mu_assert("error 16 !>> 60", sum_of_multiples(16) == 60);
+    return 0;
+}
+
+static char * test_19() {
+    mu_assert("error 19 !>> 78", sum_of_multiples(19) == 78);
+    return 0;
+}
+
+static char * test_31 () {
+    mu_assert("error 31 !>> 225", sum_of_multiples(31) == 225);
+    return 0;
+}
+
+static char * test_33() {
+    mu_assert("error 33 !>> 225", sum_of_multiples(33) == 225);
+    return 0;
+}
+
+static char * test_48() {
+    mu_assert("error 48 !>> 543", sum_of_multiples(47) == 495);
+    return 0;
+}
+
+static char * test_1000() {
+    mu_assert("error 1000 !>> 233,168", sum_of_multiples(1000) == 233168);
+    return 0;
+}
+*/
+
+static char * all_tests() {
+    mu_run_test(test_10);
+    mu_run_test(test_50);
+
+    return 0;
+}
