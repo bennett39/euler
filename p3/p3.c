5da3983200dfcad28614d27ce1c5e0d0c21d8313
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-08 11:04:39 -0500

    Add test structure and debugging printf statement

--- p3/p3.c
+++ p3/p3.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include "../minunit.h"
 
 /* Program functions */
-
-
+int * prime_factors(long long number);
 
 /* Testing functions */
 static char * all_tests();
@@ -38,17 +38,61 @@ int main() {
 
 /* PROGRAM */
 
+/* Returns a pointer to the single-dimension array `factors` */
+/* Number argument goes up to 1 trillion, so needs to be a long long. */ 
+int * prime_factors(long long number) {
+
+    /* Array size of 40 to accommodate prime factors up to 1 trillion, since
+     * log2(1 trillion) ~= 39 -- the largest possible array of prime
+     * factors less than 1 trillion. (e.g. 2 ^ 39) */
+    static int factors[40];
+
+    printf("%lli: {", number);
+    
+    int i;
+    for (i = 0; i < 40; i++) {
+        if (factors[i] != 0) {
+            printf("%d, ", factors[i]);
+        }
+    }
+
+    printf("}\n");
+
+    return factors;
+}
 
 
 /* TESTS */
-
+/*
 static char * test_tk() {
     mu_assert("",);
     return 0;
 }
+*/
+static char * test_0() {
+    int *a;
+    a = prime_factors(0);
+    int b[40] = {0};
+    
+    mu_assert("0 should give {0, ...}", \
+            memcmp(a, b, sizeof(a)) == 0);
+    return 0;
+}
+
+static char * test_13195() {
+    int *a;
+    a = prime_factors(13195);
+    int b[40] = {5, 7, 13, 29};
+
+    mu_assert("13195 should give {5, 7, 13, 29, ...}", \
+            memcmp(a, b, sizeof(a)) == 0);
+    return 0;
+}
 
 static char * all_tests() {
-    mu_run_test(test_tk);
+    /* mu_run_test(test_tk); */
+    mu_run_test(test_0);
+    mu_run_test(test_13195);
 
     return 0;
 }
