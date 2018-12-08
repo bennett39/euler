fedcdcf80d43235ebdac0bd36b9f04e675c9be69
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-08 17:02:50 -0500

    Implement double for-loop solution

--- p4/p4.c
+++ p4/p4.c
@@ -7,36 +7,11 @@
 # include "../minunit.h"
 
 /* Program functions */
-int largest_palindrome_product(int digits);
-
-/* Testing functions */
-static char * all_tests();
-
-/* Test counter. Incremented by minunit.h */
-int tests_run = 0;
+int largest_palindrome_product();
 
 int main() {
-    /* 
-     * The reults of all_tests() from minunit.h will == 0 when all 
-     * tests pass.
-     * Otherwise, all_tests() will return a string describing the 
-     * failure. 
-    */
-    char *result = all_tests();
-    if (result != 0) {
-        printf("%s\n", result);
-    }
-    else {
-        printf("ALL TESTS PASSED\n");
-    }
-    
-    /* 
-     * minunit.h counts the number of tests it ran, in order listed in
-     * all_tests() below, until a test fails and all_tests quits. 
-    */
-    printf("Tests run %d\n", tests_run);
-    
-    return result != 0;
+    printf("%d\n", largest_palindrome_product());
+    return 0;
 }
 
 /* PROGRAM */
@@ -44,6 +19,8 @@ int is_palindrome(int x) {
     int original = x;
     int reverse = 0;
 
+    /* Uses modulo and base-10 division/multiplication to reverse an 
+     * int */
     while (original > 0) {
         reverse *= 10;
         
@@ -52,6 +29,7 @@ int is_palindrome(int x) {
         original /= 10;
     }
 
+    /* Does the reversed int match the input int? */
     if (reverse == x) {
         return 1;
     }
@@ -59,33 +37,33 @@ int is_palindrome(int x) {
     return 0;
 }
 
+int largest_palindrome_product() {
+    int a, b;
+    int max = 0;
 
-int largest_palindrome_product(int digits) {
-    int max = pow(pow(10, digits), 2) - 1;
-    
-    printf("%d\n", max);
-    if (is_palindrome(max) == 1) {
-        printf("%d is a palindrome.\n", max);
+    /* Use double for loop to check all values of a and b. Worst case
+     * runs in quadratic O(n^2) time, but includes a break statement to
+     * avoid that once a palindrome is found. 
+     *
+     * Easily extendable to larger values of a and b, but need to make
+     * `product` a long long data type. */
+    for (a = 999; a > 100; a--) {
+        for (b = 999; b > 100; b--) {
+            int product = a * b;
+            
+            /* The product won't get any bigger in this loop, so break
+             * to outer loop */
+            if (product < max) {
+                break;
+            }
+
+            /* The loop would have broken if the product was smaller
+             * than the max, so product must be the new max. */
+            if (is_palindrome(product) == 1) {
+                max = product;
+            }
+        }
     }
+
     return max;
-}
-
-/* TESTS */
-/*
-static char * test_tk() {
-    mu_assert("",);
-    return 0;
-}
-*/
-
-static char * test_tk() {
-    mu_assert("2 digits should be 9009", \
-            largest_palindrome_product(2) == 9009);
-    return 0;
-}
-
-static char * all_tests() {
-    mu_run_test(test_tk);
-
-    return 0;
 }
