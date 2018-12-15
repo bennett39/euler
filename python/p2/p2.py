bfe01f0e65120c99a91b8729e7f853a69c270b58
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-15 14:17:09 -0500

    Add summary comment

--- python/p2/p2.py
+++ python/p2/p2.py
@@ -1,4 +1,13 @@
 def even_fibonaccis(last, current, total, upper_bound):
+    """
+    A function to calculate the sum of even fibonacci numbers below an
+    upper bound.
+
+    Recursively finds a new Fibonacci number and adds it to the total if
+    it's even.
+
+    Runtime: O(upper_bound) - linear
+    """
     new = last + current
 
     if new > upper_bound:
@@ -16,6 +25,7 @@ def even_fibonaccis(last, current, total, upper_bound):
 def main():
     print(even_fibonaccis(1, 2, 2, 4000000))
     return 0
+
 
 if __name__ == "__main__":
     main()
