cdd3eac1dcef38df6eaf66c98ac83369007b94e2
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-18 11:29:17 -0500

    Implement working straightforward solution

--- c/p10/p10.c
+++ c/p10/p10.c
@@ -35,10 +35,28 @@ int main() {
 
 /* PROGRAM */
 unsigned int* primes_below_root(unsigned long n) {
-    static unsigned int primes[200];
+    /* Return array size of 300 b/c program designed with 2 million as 
+     * upper bound. primes_below_root will go up to sqrt(2mil), so less 
+     * than 1,500. Not likely to find more than 300 primes below 1,500. 
+     * */
+    static unsigned int primes[300] = {2};
+    unsigned int root = sqrt(n);
+
+    int i = 3;
+    int counter = 1;
     
-    primes[0] = 12;
-    primes[1] = 3;
+    while (i <= root) {
+        for (int j = 0; j < 300; j++) {
+            if (j == counter) {
+                primes[counter] = i;
+                counter++;
+            }
+            if (i % primes[j] == 0) {
+                break;
+            }
+        }
+        i += 2;
+    }
 
     return primes;
 }
@@ -46,23 +64,58 @@ unsigned int* primes_below_root(unsigned long n) {
 
 /* Dealing with summation of millions. Use long long. */
 unsigned long long summation_primes(unsigned long upper_bound) {
-    unsigned int* factors = primes_below_root(upper_bound);
     unsigned long long sum = 0;
 
-    printf("%d, %d\n", factors[0], factors[1]);
-    
+    if (upper_bound > 3) {
+        sum = 2;
+        
+        unsigned int* factors = primes_below_root(upper_bound);
+        unsigned long i = 3;
+
+        while (i < upper_bound) {
+            for (int j = 0; j < 300; j++) {
+                if (factors[j] == 0 || factors[j] == i) {
+                    sum += i;
+                    break;
+                }
+                else if (i % factors[j] == 0) {
+                    break;
+                }
+            }
+            i += 2;
+        }
+    }
+    printf("%llu\n", sum); 
     return sum;
 }
 
 
 /* TESTS */
-static char * test_tk() {
-    mu_assert("", summation_primes(0) == 0);
+static char * test_0() {
+    mu_assert("Failed 0", summation_primes(0) == 0);
+    return 0;
+}
+
+static char * test_10() {
+    mu_assert("Failed 10", summation_primes(10) == 17);
+    return 0;
+}
+
+static char * test_100() {
+    mu_assert("Failed 100", summation_primes(100) == 1060);
+    return 0;
+}
+
+static char * test_2mil() {
+    mu_assert("Failed 2 mil", summation_primes(2000000) == 142913828922);
     return 0;
 }
 
 static char * all_tests() {
-    mu_run_test(test_tk);
+    mu_run_test(test_0);
+    mu_run_test(test_10);
+    mu_run_test(test_100);
+    mu_run_test(test_2mil);
 
     return 0;
 }
