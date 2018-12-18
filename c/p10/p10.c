9d3bc7b70591d22f98b21d1a25df0b15d5055412
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-18 09:24:26 -0500

    Implement barebones framework for p10

---
+++ c/p10/p10.c
@@ -0,0 +1,48 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "../minunit.h"
+
+/* Testing function prototype */
+static char * all_tests();
+
+/* Test counter. Incremented by minunit.h */
+int tests_run = 0;
+
+int main() {
+    /* 
+     * The reults of all_tests() from minunit.h will == 0 when all 
+     * tests pass. Otherwise, all_tests() will return a string 
+     * describing the failure. 
+    */
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    
+    /* 
+     * minunit.h counts the number of tests it ran, in order listed in
+     * all_tests() below, until a test fails and all_tests quits. 
+    */
+    printf("Tests run %d\n", tests_run);
+    
+    return result != 0;
+}
+
+/* PROGRAM */
+
+
+
+/* TESTS */
+static char * test_tk() {
+    mu_assert("", 0 == 0);
+    return 0;
+}
+
+static char * all_tests() {
+    mu_run_test(test_tk);
+
+    return 0;
+}
