e8976c00fcc0dc2730ef20dc90c5230b94bf6792
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2018-12-21 16:19:09 -0500

    Implement triangle_number_divisibility() working solution

--- c/p12/p12.c
+++ c/p12/p12.c
@@ -36,44 +36,78 @@ int main() {
 /* PROGRAM */
 /* Idk how long the input might need to be, so long long for now. */
 int number_of_divisors(unsigned long long x) {
-    if (x <= 0) {
-        return 0;
-    }
-    else if (x > 0) {
-        int divisors = 0;
-        unsigned long root = sqrt(x);
+    int divisors = 0;
+    unsigned long root = sqrt(x);
+    unsigned long i;
 
-        unsigned long i;
-        for (i = 1; i <= root; i++) {
-            if (x % i == 0) {
-                unsigned long complement = x / i;
-                if (i == complement) {
-                    divisors++;
-                }
-                else {
-                    divisors += 2;
-                }
+    for (i = 1; i <= root; i++) {
+        if (x % i == 0) {
+            if (i == x/i) {
+                divisors++;
+            }
+            else {
+                divisors += 2;
             }
         }
-        return divisors;
+    }
+    return divisors;
+}
+
+unsigned long long triangle_number_divisibility(int divisors) {
+    if (divisors < 1) {
+        return 0;
+    }
+    else if (divisors >= 1) {
+        unsigned long long triangle_number = 1;
+        unsigned long long i;
+
+        for (i = 2; ; i++) {
+            if (number_of_divisors(triangle_number) > divisors) {
+                return triangle_number;
+            }
+            else {
+                triangle_number += i;
+            }
+        }
     }
 }
 
 
 /* TESTS */
 static char * test_divisors_1() {
-    mu_assert("Error: 1 !>> 1", number_of_divisors(1) == 1);
+    mu_assert("number_of_divisors(1) != 1", number_of_divisors(1) == 1);
     return 0;
 }
 
 static char * test_divisors_6() {
-    mu_assert("Error: 6 !>> 4", number_of_divisors(6) == 4);
+    mu_assert("number_of_divisors(6) != 4", number_of_divisors(6) == 4);
+    return 0;
+}
+
+static char * test_trinum_1() {
+    mu_assert("triangle_number_divisibility(1) != 3", \
+            triangle_number_divisibility(1) == 3);
+    return 0;
+}
+
+static char * test_trinum_5() {
+    mu_assert("triangle_number_divisibility(5) != 28", \
+            triangle_number_divisibility(5) == 28);
+    return 0;
+}
+
+static char * test_trinum_500() {
+    mu_assert("triangle_number_divisibility(500) != 76,576,500", \
+            triangle_number_divisibility(500) == 76576500);
     return 0;
 }
 
 static char * all_tests() {
     mu_run_test(test_divisors_1);
     mu_run_test(test_divisors_6);
+    mu_run_test(test_trinum_1);
+    mu_run_test(test_trinum_5);
+    mu_run_test(test_trinum_500);
 
     return 0;
 }
